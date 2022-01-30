2b565a73d233173623165293ee7324e429b92c06
Prashant2344 <41367474+Prashant2344@users.noreply.github.com>
2022-01-30 14:22:52 +0545

    Book display list created

---
+++ display.php
@@ -0,0 +1,51 @@
+<?php
+include 'connection.php';
+    $q = "select * from book";
+    $query = mysqli_query($con,$q);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Kotuto Test</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <div class="col-lg-12">
+        <h1 class="text-center">Book List</h1>
+        <button class="btn btn-success"> <a href="insert.php" class="text-white"> Create</a></button>
+        <table class="table table-striped table-hover table-bordered">
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Action</th>
+            </tr>
+
+            <?php
+                while($res = mysqli_fetch_array($query)){
+            ?>
+            <tr>
+                <td><?php echo $res['title'] ?></td>
+                <td><?php echo $res['author'] ?></td>
+                <td>
+                    <button class="btn btn-success"> <a href="update.php?id= <?php echo $res['id']; ?>" class="text-white"> Update</a></button>
+                    <button class="btn btn-danger"> <a href="delete.php?id= <?php echo $res['id']; ?>" class="text-white"> Delete</a></button>
+                </td>
+            </tr>
+            <?php
+                }
+            ?>
+        </table>
+    </div>
+</div>
+
+</body>
+</html>

dab372685d9c9d6491ac4bc4737285e90ed6dacb
Prashant2344 <41367474+Prashant2344@users.noreply.github.com>
2022-01-31 20:25:30 +0545

    website pages designs

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Kotuto Test</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kotuko Test</a>
+                </li>
+            </ul>
+            <span class="navbar-text">
+                Navbar text with an inline element
+            </span>
+        </div>
+    </nav>
+
+    <div class="container" style="margin-top: 50px;">
+
+        <div class="row">
+            <h2>Kotuko Books</h2>
+        </div>
+        <div class="row mt-2">
+            <div class="col-md-3 mb-2 mt-2">
+                <div class="card">
+                    <img src="https://purnapaath.com/uploads/%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE%E0%A4%AE%E0%A5%87%E0%A4%B0%E0%A4%BE.png" alt="" style="height:250px;width:100%;object-fit:cover;">
+                    <p style="text-align: center;"><a href=""> Book Title </a></p>
+                </div>
+            </div>
+            <div class="col-md-3 mb-2 mt-2">
+                <div class="card">
+                    <img src="https://purnapaath.com/uploads/%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE%E0%A4%AE%E0%A5%87%E0%A4%B0%E0%A4%BE.png" alt="" style="height:250px;width:100%;object-fit:cover;">
+                    <p style="text-align: center;"><a href=""> Book Title </a></p>
+                </div>
+            </div>
+            <div class="col-md-3 mb-2 mt-2">
+                <div class="card">
+                    <img src="https://purnapaath.com/uploads/%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE%E0%A4%AE%E0%A5%87%E0%A4%B0%E0%A4%BE.png" alt="" style="height:250px;width:100%;object-fit:cover;">
+                    <p style="text-align: center;"><a href=""> Book Title </a></p>
+                </div>
+            </div>
+            <div class="col-md-3 mb-2 mt-2">
+                <div class="card">
+                    <img src="https://purnapaath.com/uploads/%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE%E0%A4%AE%E0%A5%87%E0%A4%B0%E0%A4%BE.png" alt="" style="height:250px;width:100%;object-fit:cover;">
+                    <p style="text-align: center;"><a href=""> Book Title </a></p>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-2 mt-2">
+                <div class="card">
+                    <img src="https://purnapaath.com/uploads/%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE%E0%A4%AE%E0%A5%87%E0%A4%B0%E0%A4%BE.png" alt="" style="height:250px;width:100%;object-fit:cover;">
+                    <p style="text-align: center;"><a href=""> Book Title </a></p>
+                </div>
+            </div>
+            <div class="col-md-3 mb-2 mt-2">
+                <div class="card">
+                    <img src="https://purnapaath.com/uploads/%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE%E0%A4%AE%E0%A5%87%E0%A4%B0%E0%A4%BE.png" alt="" style="height:250px;width:100%;object-fit:cover;">
+                    <p style="text-align: center;"><a href=""> Book Title </a></p>
+                </div>
+            </div>
+            <div class="col-md-3 mb-2 mt-2">
+                <div class="card">
+                    <img src="https://purnapaath.com/uploads/%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE%E0%A4%AE%E0%A5%87%E0%A4%B0%E0%A4%BE.png" alt="" style="height:250px;width:100%;object-fit:cover;">
+                    <p style="text-align: center;"><a href=""> Book Title </a></p>
+                </div>
+            </div>
+            <div class="col-md-3 mb-2 mt-2">
+                <div class="card">
+                    <img src="https://purnapaath.com/uploads/%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE%E0%A4%AE%E0%A5%87%E0%A4%B0%E0%A4%BE.png" alt="" style="height:250px;width:100%;object-fit:cover;">
+                    <p style="text-align: center;"><a href=""> Book Title </a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
